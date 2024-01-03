fdfe8f911eddfdc0c99e2469f677bf1eb5217326
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-03 15:43:59 +0100

    Revert "add add react router and a PageNotFound component"

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomeView } from './components/HomeView';
 import { LoginPage } from './components/LoginPage';
 import { Navbar } from './components/Navbar';
 import { ProfileView } from './components/ProfileView';
-import { PageNotFound } from './components/PageNotFound';
 
 function App() {
   return (
     <div className="app">
       <Navbar></Navbar>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginPage></LoginPage>}></Route>
-          <Route path='/home' element={<HomeView></HomeView>}></Route>
-          <Route path='/profile/:userId' element={<ProfileView></ProfileView>}></Route>
-          <Route path="*" element={<PageNotFound></PageNotFound>} />
-        </Routes>
-      </BrowserRouter>
+      <LoginPage></LoginPage>
+      <HomeView></HomeView>
+      <ProfileView></ProfileView>
     </div>
   );
 }

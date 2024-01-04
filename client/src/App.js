61ae92375e97e97ea6c925eee0bbd4c493284297
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-04 14:22:27 +0100

    Setup MUI theme

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,26 @@ import { LoginPage } from './components/LoginPage';
 import { Navbar } from './components/Navbar';
 import { ProfileView } from './components/ProfileView';
 import { PageNotFound } from './components/PageNotFound';
-
+import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
+import { themeSettings } from './theme/theme';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <Navbar></Navbar>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginPage></LoginPage>}></Route>
-          <Route path='/home' element={<HomeView></HomeView>}></Route>
-          <Route path='/profile/:userId' element={<ProfileView></ProfileView>}></Route>
-          <Route path="*" element={<PageNotFound></PageNotFound>} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path='/' element={<LoginPage></LoginPage>}></Route>
+            <Route path='/home' element={<HomeView></HomeView>}></Route>
+            <Route path='/profile/:userId' element={<ProfileView></ProfileView>}></Route>
+            <Route path="*" element={<PageNotFound></PageNotFound>} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );

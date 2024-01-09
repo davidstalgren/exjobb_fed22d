4188592f65acb6c0c4fa73738eef9cf69d04cc9f
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-09 11:18:22 +0100

    tried a route to add user to db via Postman

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log('get user route was invoked');
   res.send('route is working, GPN is coming along!');
 });
+
+router.post('/addUser', async(req, res) => {
+  try {
+    const {firstName, lastName, email, password} = req.body;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUserToAdd = new User({firstName, lastName, email, "password": hashedPassword});
+    await newUserToAdd.save();
+    console.log(newUserToAdd, `added a new user to DB`);
+    res.status(201).json(newUserToAdd);
+  } catch(error) {
+    console.log(error);
+    res.status(400).json({ error: 'Something went wrong' });
+  }
+})
 
 module.exports = router;

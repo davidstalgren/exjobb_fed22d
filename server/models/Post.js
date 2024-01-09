d3c5b8b41f1d97cc6758b9b4c1d2f112df5fbf74
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-09 09:19:23 +0100

    add setup for the PostSchema model for mongoose

---
+++ server/models/Post.js
@@ -0,0 +1,34 @@
+const { mongoose } = require("mongoose");
+
+const PostSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true
+    },
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 30
+    },
+    location: String,
+    userPictrureUrl: String,
+    content: String,
+    contentPictureUrl: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: []
+    }
+  }
+);
+
+module.exports = mongoose.model('Post', PostSchema);

513c2cec521e5dfe88a295b9223ce306bebad078
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-12 15:39:39 +0100

    getFriends route done

--- server/routes/users.js
+++ server/routes/users.js
@@ -16,6 +16,30 @@ router.get('/:id', verifyToken, async(req, res) => {
     console.log(error);
     res.status(400).json({ error: 'Something went wrong' });
   }
+});
+
+router.get('/:id/friends', verifyToken, async(req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    console.log(user);
+    if(user) {
+      const friendsListFull = await Promise.all(
+        user.friends.map((id) => User.findById(id))
+      );
+
+      const friendsListMinified = friendsListFull.map(({_id, firstName, lastName, location, pictureUrl}) => {
+        return {_id, firstName, lastName, location, pictureUrl}
+      });
+      res.status(200).json(friendsListMinified);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch(error) {
+    console.log(error);
+    res.status(400).json({ error: 'Something went wrong' });
+  }
 })
+
+
 
 module.exports = router;

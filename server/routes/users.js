19e39aeca9e7bc4a0dc8fd4962df6b696863f52f
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-16 22:29:10 +0100

    add the addRemoveFriend route

--- server/routes/users.js
+++ server/routes/users.js
@@ -40,6 +40,34 @@ router.get('/:id/friends', verifyToken, async(req, res) => {
     console.log(error);
     res.status(400).json({ error: 'Something went wrong' });
   }
-})
+});
+
+/* -------------------------------
+-------Add or Remove Friend-------
+------------------------------- */
+router.put('/:id/:friendId', verifyToken, async(req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const friend = await User.findById(req.params.friendId);
+
+    if (user.friends.includes(req.params.friendId)) {
+      user.friends = user.friends.filter((id) => id !== req.params.friendId);
+      friend.friends = friend.friends.filter((id) => id !== req.params.id);
+    } else {
+      user.friends.push(req.params.friendId);
+      friend.friends.push(req.params.id);
+    }
+    await user.save();
+    await friend.save();
+
+    const updatedFriendsList = await Promise.all(
+      user.friends.map((id) => User.findById(id).select('_id firstName lastName location pictureUrl'))
+    );
+    res.status(200).json(updatedFriendsList)
+  } catch(error) {
+    console.log(error);
+    res.status(400).json({ error: 'Something went wrong' });
+  }
+});
 
 module.exports = router;

f849fca7b6b1f28986eae1292c13d92dd713817d
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-17 14:38:47 +0100

    add route to like/unlike post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -29,6 +29,27 @@ router.get('/:userId', verifyToken, async(req, res) => {
   }
 });
 
+/* -------------------------------
+------------Like Post-------------
+------------------------------- */
+router.put('/:postId/like', verifyToken, async(req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    const likeStatus = post.likes.get(req.body.userId);
 
+    if(likeStatus) {
+      post.likes.delete(req.body.userId);
+    } else {
+      post.likes.set(req.body.userId, true);
+    }
+    await post.save();
+    const newPostStatus = await Post.findById(req.params.postId);
+    res.status(200).json(newPostStatus);
+
+  } catch(error) {
+    console.log(error);
+    res.status(400).json({ error: 'Something went wrong' });
+  }
+});
 
 module.exports = router;

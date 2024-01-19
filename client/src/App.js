6013d17b72d87d9105f66e80c0cac2afb6208dfd
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-19 22:52:01 +0100

    remove navbar from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomeView } from './components/HomeView';
 import { LoginPage } from './components/LoginPage';
-import { Navbar } from './components/Navbar';
 import { ProfileView } from './components/ProfileView';
 import { PageNotFound } from './components/PageNotFound';
 import { useSelector } from 'react-redux';
@@ -14,7 +13,6 @@ function App() {
 
   return (
     <div className="app">
-      <Navbar></Navbar>
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <Routes>

a4b900231da90c719bc07cd2be68856f1f3f7c05
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-19 23:20:05 +0100

    add responsive heading and logos to loginpage

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,7 +1,21 @@
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material"
+import { BoxSpaced } from "./styled/StyledBox";
+import { Logo } from "./Logo";
+
 export function LoginPage() {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery('(min-width: 900px)');
+
   return (
-    <>
-      <div>LoginPage component</div>
-    </>
+    <Box>
+      <BoxSpaced backgroundColor={theme.palette.background.alt}>
+        {isDesktop ? <Logo width={75} /> : <Logo width={50} />}
+          <Typography align="center" variant="h1" color="primary" fontWeight='900' fontSize='clamp(1rem, 3vw, 4rem)'>
+            GreenPatch Network
+          </Typography>
+        {isDesktop ? <Logo width={75} /> : <Logo width={50} />}
+      </BoxSpaced>
+
+    </Box>
   )
 }

63a0ed4cde104e09efce1cd150f3144a1b762705
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-22 15:42:58 +0100

    Add dropzone for profile picture upload. Also email and password inputs.

--- client/src/components/LoginRegForm.jsx
+++ client/src/components/LoginRegForm.jsx
@@ -1,15 +1,19 @@
 import {object, string} from 'yup';
 import { Formik } from 'formik';
 import { useState } from 'react';
-import { Box, TextField, useMediaQuery, useTheme } from '@mui/material';
+import { Box, TextField, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import Dropzone from "react-dropzone";
+import { BoxSpaced } from './styled/StyledBox';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
 
 const registerUserSchema = object({
   firstName: string().min(2, 'Too Short!').max(30, 'Too Long!').required('First name is required'),
   lastName: string().min(2, 'Too Short!').max(30, 'Too Long!').required('Last name is required'),
   email: string().email('Email is not a valid email').required('Email is required'),
-  password: string().min(8, 'Password needs to be atleast 8 characters!').max(50, 'Too Long!!! Think 50 should be enough').required('Need to add a password'),
+  password: string().min(8, 'Password needs to be atleast 8 characters!').max(50, 'Too Long!!! Think 50 characters should be enough').required('Need to add a password'),
   picture: string().required('Need to add a Profile picture'),
   location: string().required('Where are you from?')
 });
@@ -20,9 +24,13 @@ const loginUserSchema = object({
 });
 
 export function LoginRegForm() {
+  //eslint-disable-next-line
   const [activeView, setActiveView] = useState('register');
+  //eslint-disable-next-line
   const theme = useTheme();
+  //eslint-disable-next-line
   const dispatch = useDispatch();
+  //eslint-disable-next-line
   const navigate = useNavigate();
   const isDesktop = useMediaQuery('(min-width:700px)');
   
@@ -48,7 +56,28 @@ export function LoginRegForm() {
                   <TextField label='First Name' onBlur={handleBlur} onChange={handleChange} value={values.firstName} name='firstName' error={Boolean(touched.firstName) && Boolean(errors.firstName)} helperText={touched.firstName && errors.firstName} sx={{gridColumn: 'span 1'}}></TextField>
                   <TextField label='Last Name' onBlur={handleBlur} onChange={handleChange} value={values.lastName} name='lastName' error={Boolean(touched.lastName) && Boolean(errors.lastName)} helperText={touched.lastName && errors.lastName} sx={{gridColumn: 'span 1'}}></TextField>
                   <TextField label='Location' onBlur={handleBlur} onChange={handleChange} value={values.location} name='location' error={Boolean(touched.location) && Boolean(errors.location)} helperText={touched.location && errors.location} sx={{gridColumn: 'span 2'}}></TextField>
-                  <TextField label='Email' onBlur={handleBlur} onChange={handleChange} value={values.email} name='email' error={Boolean(touched.email) && Boolean(errors.email)} helperText={touched.email && errors.email} sx={{gridColumn: 'span 2'}}></TextField>
+                  <Box sx={{gridColumn: 'span 2'}}>
+                    <Dropzone multiple={false} onDrop={(acceptedFiles) => setFieldValue("picture", acceptedFiles[0])}>
+                      {({ getRootProps, getInputProps }) => (
+                        <Box {...getRootProps()} border={`1px dotted ${theme.palette.neutral.medium}`} padding='1rem' borderRadius='0.25rem' sx={{ '&:hover': { cursor: 'pointer' } }}>
+                          <input {...getInputProps()} />
+                          {!values.picture ? (
+                            <BoxSpaced>
+                              <Typography>Add your profile picture here, click or drag and drop</Typography>
+                              <InsertPhotoIcon />
+                            </BoxSpaced>
+                          ) : (
+                            <BoxSpaced>
+                              <Typography>{values.picture.name}</Typography>
+                              <ModeEditIcon />
+                            </BoxSpaced>
+                          )}
+                        </Box>
+                      )}
+                    </Dropzone>
+                  </Box>
+                  <TextField label='Email' onBlur={handleBlur} onChange={handleChange} value={values.email} name='email' error={Boolean(touched.email) && Boolean(errors.email)} helperText={touched.email && errors.email} autoComplete='' sx={{gridColumn: 'span 2'}}></TextField>
+                  <TextField type='password' label='Password' onBlur={handleBlur} onChange={handleChange} value={values.password} name='password' error={Boolean(touched.password) && Boolean(errors.password)} helperText={touched.password && errors.password} sx={{gridColumn: 'span 2'}}></TextField>
                 </>
               )}
             </Box>

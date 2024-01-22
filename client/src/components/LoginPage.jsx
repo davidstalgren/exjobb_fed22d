77efd5be86be8f78f083d0abd49764cf04e84b05
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-22 15:41:17 +0100

    change fontSize clamp to have rem as preffered value too for better accessability.

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -11,13 +11,13 @@ export function LoginPage() {
     <Box>
       <BoxSpaced backgroundColor={theme.palette.background.alt} width='100%'>
         {isDesktop ? <Logo width={75} /> : <Logo width={50} />}
-          <Typography align="center" variant="h1" color="primary" fontWeight='900' fontSize='clamp(1rem, 3vw, 4rem)'>
+          <Typography align="center" variant="h1" color="primary" fontWeight='900' fontSize='clamp(1rem, 2vw + 1rem, 4rem)'>
             GreenPatch Network
           </Typography>
         {isDesktop ? <Logo width={75} /> : <Logo width={50} />}
       </BoxSpaced>
       <Box width={isDesktop ? '50%' : '90%'} padding='1.5rem' margin='1.5rem auto' borderRadius='1rem' backgroundColor={theme.palette.background.alt}>
-        <Typography align='center' fontSize='clamp(1rem, 1.5vw, 1.25rem)' marginBottom='1rem'>
+        <Typography align='center' fontSize='clamp(1rem, 0.5vw + 1rem, 1.25rem)' marginBottom='1rem'>
           Welcome GreenPatch Friend! Log in to share your knowledge of how to get the perfect lawn.
         </Typography>
         <LoginRegForm></LoginRegForm>

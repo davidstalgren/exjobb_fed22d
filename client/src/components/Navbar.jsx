8fbe995612eea19eab6f254f39127049997de8eb
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-23 23:04:55 +0100

    add menu and theme toggle

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
-import { useMediaQuery, useTheme, Typography } from "@mui/material";
+import { useMediaQuery, useTheme, Typography, Box, Divider } from "@mui/material";
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom"
-import { BoxSpaced } from "./styled/StyledBox";
+import { BoxSpaced, BoxSpacedColumn } from "./styled/StyledBox";
 import { Logo } from "./Logo";
 import IconButton from '@mui/material/IconButton';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import { setMode } from "../store/reducers/reducers";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { setLogout, setMode } from "../store/reducers/reducers";
 
 export function Navbar() {
   const navigate = useNavigate();
@@ -17,7 +19,7 @@ export function Navbar() {
   const isDesktop = useMediaQuery('(min-width: 900px)');
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
-    <BoxSpaced BoxSpaced padding='1rem 3rem' backgroundColor={theme.palette.background.alt}>
+    <BoxSpaced padding={isDesktop ? '1rem 3rem' : '1rem'} backgroundColor={theme.palette.background.alt}>
       <BoxSpaced gap='1rem'>
         {isDesktop ? <Logo width={50} /> : <Logo width={40} />}
         <Typography align="center" variant="h1" color="primary" fontWeight='900' fontSize='clamp(1rem, 1vw + 1rem, 2.5rem)'>
@@ -26,15 +28,45 @@ export function Navbar() {
       </BoxSpaced>
 
       {isDesktop ? (
-        <BoxSpaced>
+        <BoxSpaced gap='2rem'>
           <IconButton onClick={() => dispatch(setMode())}>
             {theme.palette.mode === 'light' ? <DarkModeOutlinedIcon /> : <LightModeOutlinedIcon />}
           </IconButton>
-          <p>menu/user logout</p>
-
+          <BoxSpacedColumn>
+            <Box component='img' src={`${process.env.REACT_APP_API_URL}/assets/${user.pictureUrl}`} alt='Preview of chosen file' width='3rem' height='3rem' borderRadius='50%' sx={{objectFit: 'cover'}}/>
+            <Typography onClick={() => {
+              dispatch(setLogout());
+              navigate('/')
+              }} sx={{textDecoration: 'underline', color: theme.palette.primary.dark, '&:hover': {cursor: 'pointer', color: theme.palette.primary.main}}}>
+                Log out
+            </Typography>
+          </BoxSpacedColumn>
         </BoxSpaced>
-      ) : <p>mobile view</p>}
-      
+      ) : (
+        <IconButton onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+          <MenuIcon></MenuIcon>
+        </IconButton>
+      )}
+      {mobileMenuOpen && !isDesktop && (
+        <Box display='flex' flexDirection='column' justifyContent='flex-start' alignItems='center' gap='1rem' padding='1rem' position='fixed' top='0' right='0' width='200px' height='100%' backgroundColor={theme.palette.primary.light}>
+          <BoxSpaced width='100%' paddingLeft='2rem'>
+          <Box component='img' src={`${process.env.REACT_APP_API_URL}/assets/${user.pictureUrl}`} alt='Preview of chosen file' width='3rem' height='3rem' borderRadius='50%' sx={{objectFit: 'cover'}}/>
+          <IconButton onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+            <CloseIcon></CloseIcon>
+          </IconButton>
+          </BoxSpaced>
+          <Divider color='primary' flexItem />
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === 'light' ? <DarkModeOutlinedIcon /> : <LightModeOutlinedIcon />}
+          </IconButton>
+          <Typography onClick={() => {
+            dispatch(setLogout());
+            navigate('/');
+            }} sx={{textDecoration: 'underline', color: theme.palette.primary.dark, '&:hover': {cursor: 'pointer', color: theme.palette.primary.main}}}>
+            Log out
+          </Typography>
+        </Box>
+      )}
     </BoxSpaced>
   )
 }

e2de62577ed1cb13f857c705115b89df314150f8
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-25 14:50:59 +0100

    add layout to HomeView component

--- client/src/components/HomeView.jsx
+++ client/src/components/HomeView.jsx
@@ -3,16 +3,29 @@ import { FriendsListWrapper } from "./FriendsListWrapper";
 import { PostsWrapper } from "./PostsWrapper";
 import { UserWrapper } from "./UserWrapper";
 import { Navbar } from "./Navbar";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 
 export function HomeView() {
+  const isDesktop = useMediaQuery('(min-width: 900px)');
+  const user = useSelector((state) => state.user);
+  
   return (
-    <>
+    <Box>
       <Navbar></Navbar>
-      <div>HomeView component</div>
-      <UserWrapper></UserWrapper>
-      <CreatePostWrapper></CreatePostWrapper>
-      <PostsWrapper></PostsWrapper>
-      <FriendsListWrapper></FriendsListWrapper>
-    </>
+      <Box width='100%' padding='2rem' display={isDesktop ? 'flex' : 'block'} gap='0.75rem' justifyContent='space-between'>
+        <Box flexBasis={isDesktop ? '25%' : undefined}>
+          <UserWrapper></UserWrapper>
+        </Box>
+        <Box flexBasis={isDesktop ? '45%' : undefined}>
+          <CreatePostWrapper></CreatePostWrapper>
+          <PostsWrapper></PostsWrapper>
+        </Box>
+        {isDesktop && 
+          <Box flexBasis='25%'>
+            <FriendsListWrapper></FriendsListWrapper>
+          </Box>}
+      </Box>
+    </Box>
   )
 }

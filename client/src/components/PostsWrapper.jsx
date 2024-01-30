5063d4ad5a1821a5aa12b9f5e9e9d8b9e602e261
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-30 09:31:30 +0100

    add functions to get and set posts to state

--- client/src/components/PostsWrapper.jsx
+++ client/src/components/PostsWrapper.jsx
@@ -1,12 +1,43 @@
+import { useDispatch, useSelector } from "react-redux";
 import { SinglePostWrapper } from "./SinglePostWrapper";
+import { useEffect, useState } from "react";
+import { setPosts } from "../store/reducers/reducers";
 
-export function PostsWrapper() {
+export function PostsWrapper({profileId, profileView = false}) {
+  const dispatch = useDispatch()
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
+
+  useEffect(() => {
+    
+    async function getUserPosts() {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/posts/${profileId}`, {
+        method: 'GET',
+        headers: {Authorization: `Token ${token}`}
+      });
+      const newPosts = await response.json();
+      dispatch(setPosts({posts: newPosts}));
+    }
+    
+    async function getAllPosts() {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/posts`, {
+        method: 'GET',
+        headers: {Authorization: `Token ${token}`}
+      });
+      const newPosts = await response.json();
+      dispatch(setPosts({posts: newPosts}));
+    }
+    
+    if (profileView) {
+      getUserPosts()
+    } else {
+      getAllPosts()
+    }
+  },[])
+
   return (
     <>
-      <div>PostsWrapper component, this will contain all posts</div>
-      <SinglePostWrapper></SinglePostWrapper>
-      <SinglePostWrapper></SinglePostWrapper>
-      <SinglePostWrapper></SinglePostWrapper>
+      {posts.map(({_id, userId, firstName, lastName, location, userPictureUrl, content, contentPictureUrl, likes, comments}) => (<SinglePostWrapper key={_id} id={_id} userId={userId} firstName={firstName} lastName={lastName} location={location} userPictureUrl={userPictureUrl} content={content} contentPictureUrl={contentPictureUrl} likes={likes} comments={comments}></SinglePostWrapper>))}
     </>
   )
 }

553da702406ed6ec4e329f155e1ee20f13e94376
davidstalgren <david.stalgren@medieinstitutet.se>
2024-01-30 12:38:16 +0100

    check that all data is coming along as props

--- client/src/components/SinglePostWrapper.jsx
+++ client/src/components/SinglePostWrapper.jsx
@@ -1,8 +1,17 @@
-export function SinglePostWrapper({id, userId, firstName, lastName, location, userPictureUrl, content, contentPictureUrl, likes, comments}) {
-  console.log(id);
+export function SinglePostWrapper({ id, userId, firstName, lastName, location, userPictureUrl, content, contentPictureUrl, likes, comments }) {
+  console.log('id', id);
+  console.log('userId', userId);
+  console.log('firstName', firstName);
+  console.log('lastName', lastName);
+  console.log('location', location);
+  console.log('userPictureUrl', userPictureUrl);
+  console.log('content', content);
+  console.log('contentPictureUrl', contentPictureUrl);
+  console.log('likes', likes);
+  console.log('comments', comments);
   return (
     <>
-    <p>{id}{userId}{firstName}{lastName}{location}</p>
+      <p>{id}{userId}{firstName}{lastName}{location}{userPictureUrl}{content}{contentPictureUrl}</p>
     </>
   )
 }

7ca727f600698bc9b5499ef7d7e8560401fa172e
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-31 00:04:17 +0100

    add some rendering of the data from posts

--- client/src/components/SinglePostWrapper.jsx
+++ client/src/components/SinglePostWrapper.jsx
@@ -1,17 +1,33 @@
+import { Divider, Typography } from "@mui/material";
+import { BoxSpaced } from "./styled/StyledBox";
+import { StyledWrapper } from "./styled/StyledWrapper";
+import { Box, useTheme} from "@mui/system";
+import { ProfileImage } from "./ProfileImage";
+import PersonAddOutlinedIcon from '@mui/icons-material/PersonAddOutlined';
+import { useNavigate } from "react-router-dom";
+
 export function SinglePostWrapper({ id, userId, firstName, lastName, location, userPictureUrl, content, contentPictureUrl, likes, comments }) {
-  console.log('id', id);
-  console.log('userId', userId);
-  console.log('firstName', firstName);
-  console.log('lastName', lastName);
-  console.log('location', location);
-  console.log('userPictureUrl', userPictureUrl);
-  console.log('content', content);
-  console.log('contentPictureUrl', contentPictureUrl);
-  console.log('likes', likes);
-  console.log('comments', comments);
+  const theme = useTheme();
+  const navigate = useNavigate();
+
+
   return (
-    <>
-      <p>{id}{userId}{firstName}{lastName}{location}{userPictureUrl}{content}{contentPictureUrl}</p>
-    </>
+    <StyledWrapper margin='1rem 0rem 1rem 0rem'>
+      <BoxSpaced marginBottom='1rem'>
+        <BoxSpaced gap='1rem' onClick={() => navigate(`/profile/${userId}`)} sx={{'&:hover': {cursor: 'pointer'}}}>
+          <ProfileImage pictureUrl={userPictureUrl} size="3rem"></ProfileImage>
+          <Box>
+            <Typography color={theme.palette.primary.dark}>{firstName} {lastName}</Typography>
+          </Box>
+        </BoxSpaced>
+        <PersonAddOutlinedIcon color="primary" fontSize='large' onClick={() => {console.log('add friend');}} sx={{'&:hover': {cursor: 'pointer'}}}></PersonAddOutlinedIcon>
+      </BoxSpaced>
+      <Typography>
+        {content}
+      </Typography>
+      {contentPictureUrl && (
+        <Box component='img' src={`${process.env.REACT_APP_API_URL}/assets/${contentPictureUrl}`} alt='Post image' width='100%' borderRadius='0.25rem'/>
+      )}
+    </StyledWrapper>
   )
 }

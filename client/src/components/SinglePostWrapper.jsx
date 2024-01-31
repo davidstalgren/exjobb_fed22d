1d58d7d0f6650c74b2693a5d3bd186db05da2c5c
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-01-31 10:29:00 +0100

    add star for like

--- client/src/components/SinglePostWrapper.jsx
+++ client/src/components/SinglePostWrapper.jsx
@@ -1,15 +1,19 @@
-import { Divider, Typography } from "@mui/material";
+import { Divider, IconButton, Typography } from "@mui/material";
 import { BoxSpaced } from "./styled/StyledBox";
 import { StyledWrapper } from "./styled/StyledWrapper";
 import { Box, useTheme} from "@mui/system";
 import { ProfileImage } from "./ProfileImage";
 import PersonAddOutlinedIcon from '@mui/icons-material/PersonAddOutlined';
+import PersonRemoveOutlinedIcon from '@mui/icons-material/PersonRemoveOutlined';
 import { useNavigate } from "react-router-dom";
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import { useState } from "react";
 
 export function SinglePostWrapper({ id, userId, firstName, lastName, location, userPictureUrl, content, contentPictureUrl, likes, comments }) {
   const theme = useTheme();
   const navigate = useNavigate();
-
+  const [haveLiked, setHaveLiked] = useState(true);
 
   return (
     <StyledWrapper margin='1rem 0rem 1rem 0rem'>
@@ -20,14 +24,58 @@ export function SinglePostWrapper({ id, userId, firstName, lastName, location, u
             <Typography color={theme.palette.primary.dark}>{firstName} {lastName}</Typography>
           </Box>
         </BoxSpaced>
-        <PersonAddOutlinedIcon color="primary" fontSize='large' onClick={() => {console.log('add friend');}} sx={{'&:hover': {cursor: 'pointer'}}}></PersonAddOutlinedIcon>
+        <BoxSpaced gap='1rem'>
+          <IconButton>
+            <PersonAddOutlinedIcon 
+              color="primary" 
+              onClick={() => {console.log(`add friend ${firstName} ${lastName} with Id:${userId}`)}} 
+              sx={{'&:hover': {cursor: 'pointer'}}}>
+            </PersonAddOutlinedIcon>
+          </IconButton>
+          <IconButton>
+            <PersonRemoveOutlinedIcon 
+              color="primary" 
+              onClick={() => {console.log(`remove friend ${firstName} ${lastName} with Id:${userId}`)}} 
+              sx={{'&:hover': {cursor: 'pointer'}}}>
+            </PersonRemoveOutlinedIcon>
+          </IconButton>
+        </BoxSpaced>
       </BoxSpaced>
       <Typography>
         {content}
       </Typography>
       {contentPictureUrl && (
-        <Box component='img' src={`${process.env.REACT_APP_API_URL}/assets/${contentPictureUrl}`} alt='Post image' width='100%' borderRadius='0.25rem'/>
+        <Box 
+          component='img' 
+          src={`${process.env.REACT_APP_API_URL}/assets/${contentPictureUrl}`} 
+          alt='Post image' 
+          width='100%' 
+          borderRadius='0.25rem'/>
       )}
+      <BoxSpaced>
+        <IconButton>
+          {haveLiked ? (
+            <StarOutlinedIcon 
+              sx={{color: theme.palette.primary.dark}} 
+              onClick={() => {
+                setHaveLiked(!haveLiked); 
+                console.log(`remove like from postId: ${id}`);
+              }}>
+            </StarOutlinedIcon>
+              ) : (
+            <StarBorderOutlinedIcon 
+              sx={{color: theme.palette.primary.main}}
+              onClick={() => {
+                setHaveLiked(!haveLiked); 
+                console.log(`add like to postId: ${id}`);
+              }}>
+            </StarBorderOutlinedIcon>
+          )}
+        </IconButton>
+        <IconButton>
+
+        </IconButton>
+      </BoxSpaced>
     </StyledWrapper>
   )
 }

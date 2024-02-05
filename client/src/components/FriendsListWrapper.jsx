df584e121915c42187e92aa75616e8069effe8e0
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-02-05 23:09:55 +0100

    add the FriendsList component where mapping of UserPresentation takes place from the friendslist API request

--- client/src/components/FriendsListWrapper.jsx
+++ client/src/components/FriendsListWrapper.jsx
@@ -1,12 +1,48 @@
-import { Friend } from "./Friend";
+import { Typography } from "@mui/material";
+import { StyledWrapper } from "./styled/StyledWrapper";
+import { useDispatch, useSelector } from "react-redux";
+import { UserPresentation } from "./UserPresentation";
+import { Box } from "@mui/system";
+import { useEffect } from "react";
+import { setFriends } from "../store/reducers/reducers";
+import PersonAddOutlinedIcon from '@mui/icons-material/PersonAddOutlined';
 
-export function FriendsListWrapper() {
+export function FriendsListWrapper({userId}) {
+  const activeUser = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+
+  useEffect(() => {
+    async function getFriendsList() {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/users/${userId}/friends`, {
+        method: 'GET',
+        headers: { Authorization: `Token ${token}` },
+      });
+      const friendsList = await response.json();
+      dispatch(setFriends({friends: friendsList}));
+    }
+    getFriendsList();
+  },[])
+
   return (
-    <>
-      <div>FriendsListWrapper component, this will contain your list of friends</div>
-      <Friend></Friend>
-      <Friend></Friend>
-      <Friend></Friend>
-    </>
+    <StyledWrapper>
+      <Typography variant="h5" margin='0 0 1rem 0'>Friends</Typography>
+      <Box display='flex' flexDirection='column' gap='0.75rem'>
+        {activeUser.friends.map((friend, index) => (
+          <UserPresentation key={index} userId={friend._id} firstName={friend.firstName} lastName={friend.lastName} location={friend.location} userPictureUrl={friend.pictureUrl}></UserPresentation>
+        ))}
+      </Box>
+      {activeUser.friends.length === 0 ? (
+        <>
+          <PersonAddOutlinedIcon color="primary"></PersonAddOutlinedIcon>
+          <Typography fontSize='small'>
+            Go ahead and get some GreenPatch Friends! 
+            All you need is to click on the little Green plus icon in their posts to make new friends!
+          </Typography>
+        </>
+      ) : (
+        <></>
+      )}
+    </StyledWrapper>
   )
 }

b8e5013b119de00070e1bb9b0d3150cc33d65663
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2024-02-06 12:44:01 +0100

    add tooltip to Toggle button for light and darkmode

--- client/src/components/ThemeToggle.jsx
+++ client/src/components/ThemeToggle.jsx
@@ -1,4 +1,4 @@
-import { useTheme } from "@mui/material"
+import { Tooltip, Zoom, useTheme } from "@mui/material"
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import IconButton from '@mui/material/IconButton';
@@ -10,8 +10,10 @@ export function ThemeToggle() {
   const dispatch = useDispatch();
 
   return (
-    <IconButton onClick={() => dispatch(setMode())}>
-      {theme.palette.mode === 'light' ? <DarkModeOutlinedIcon /> : <LightModeOutlinedIcon />}
-    </IconButton>
+    <Tooltip title={theme.palette.mode === 'light' ? 'Enter darkmode' : 'Enter lightmode'} TransitionComponent={Zoom} placement="left" arrow>
+      <IconButton onClick={() => dispatch(setMode())}>
+        {theme.palette.mode === 'light' ? <DarkModeOutlinedIcon /> : <LightModeOutlinedIcon />}
+      </IconButton>
+    </Tooltip>
   )
 }
